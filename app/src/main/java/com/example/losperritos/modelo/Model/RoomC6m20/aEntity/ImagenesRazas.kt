6b5252e42b2d568ed115c847909922f71ef5bf6e
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-09-25 21:00:54 -0300

    93 % + orden y expliacion 10%

--- app/src/main/java/com/example/losperritos/modelo/Model/RoomC6m20/aEntity/ImagenesRazas.kt
+++ app/src/main/java/com/example/losperritos/modelo/Model/RoomC6m20/aEntity/ImagenesRazas.kt
@@ -1,4 +1,8 @@
 //Esta Entidad es la representacion de la tabla que se escribe en la base de datos
+//La entidad se define al escribir @Entity y es parte de room
+//las aotaciones prinicpales de room @PrimaryKey(autoGenerate = true) ,@NonNull,
+// @ColumnInfo= Sobrenombre con el que se guarda en la base de datos
+// Para acceder al crod de esta entidad(tabla) lo hacemos con la interface  b) Dao
 package com.example.losperritos.modelo.Model.RoomC6m20.aEntity
 
 
--- app/src/main/java/com/example/losperritos/modelo/Model/RoomC6m20/bDao/RazasDao.kt
+++ app/src/main/java/com/example/losperritos/modelo/Model/RoomC6m20/bDao/RazasDao.kt
@@ -1,4 +1,8 @@
 //Esta interface de Kotlin Verde, Data Acces Objet, nos proporciona los metodos Crood que nos dan acceso a los datos
+// solamenta al crear la anotacion @dato al principio, esta clase se convierte a interface
+// Esta interface solo declara metodos, pero no seimplementan,
+// Para declarar metodos, se hace declarando un nombre al metodo, lo que puede recibir y lo que devuelve C6 m53
+
 package com.example.losperritos.modelo.Model.RoomC6m20.bDao
 
 import androidx.lifecycle.LiveData
@@ -13,10 +17,17 @@ import com.example.losperritos.modelo.Model.RoomC6m20.aEntity.ListaRazas
 interface RazasDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+    //este metodo@insert(si hay conflictos = la estrategia. es reemplazar
+
     suspend fun insertAllRazas(mListaRazas: List<ListaRazas>)
+    // el atributo o palabra reservada suspend se utiliza para trabajo asincrono y es parte de corrutinas
+    // Esta funcion insertAllRazas (recive en mListaRazas un listado <de ListaRazas>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllImagesRazas(mImagenesRazas: List<ImagenesRazas>)
+    // Aunque la estrategia para 2 tablas es: 2 Entity, 2 Dao, 1 archivo database
+    // En este caso el metodo onconflict utiliza una funcion llamando al insertAllImagesRazas,
+    // pero uzando (mImagenesRazas: List<ImagenesRazas>) a diferencia del anterior
 
 
     @Query("SELECT * FROM lista_razas_table")
